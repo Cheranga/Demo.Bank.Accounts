e2893cb85c81f735ada0a2e6cc42062684859af1
Cheranga Hatangala <cheranga@gmail.com>
2022-06-12 16:39:02 +1000

    adding docker and docker-compose files to run the microservice locally.

--- Demo.Bank.Accounts.Api/Program.cs
+++ Demo.Bank.Accounts.Api/Program.cs
@@ -1,5 +1,4 @@
 using Demo.Bank.Accounts.Api;
-using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,34 +6,14 @@ var builder = WebApplication.CreateBuilder(args);
 Bootstrapper.RegisterDependencies(builder);
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-// builder.Services.AddSwaggerGen(options =>
-// {
-//     options.SwaggerDoc("Bank Accounts API", new OpenApiInfo
-//     {
-//         Version = "1.0.0",
-//         Contact = new OpenApiContact
-//         {
-//             Name = "CCHat Solutions"
-//         }
-//     });
-// });
 
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
+app.UseSwagger();
+app.UseSwaggerUI();
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
